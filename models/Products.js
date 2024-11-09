d9ff465217fbe31893af55304db4cfa9b8ed967b
alim <wajihafatima689@gmail.com>
2024-11-09 15:31:55 +0500

    .

--- models/Products.js
+++ models/Products.js
@@ -4,7 +4,7 @@ const ProductSchema = new mongoose.Schema({
    
   name: { type: String, required: true },
   description: { type: String, required: true },
-  buttontext: { type: Number, required: true },
+  buttontext: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
