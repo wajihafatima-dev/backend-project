7e95209c1e2477dca3fc4b22373a9356119e04e2
alim <wajihafatima689@gmail.com>
2024-11-09 19:19:55 +0500

    .update

--- models/cart.js
+++ models/cart.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const cartItemSchema = new mongoose.Schema({
-    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-    title: { type: String, required: true, default: 1 },
-    description: { type: String, required: true, default: 1 },
-    price: { type: Number, required: true },
-  });
-  module.exports = mongoose.model('carts', cartItemSchema);
+const CartSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('carts', CartSchema);
